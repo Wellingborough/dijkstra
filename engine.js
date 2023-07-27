79b42a381af1fb0b9ec51a4c33dbe4b2948f8296
Mark Gamble <58432939+Wellingborough@users.noreply.github.com>
2023-07-27 22:16:26 +0100

    Update engine.js

--- engine.js
+++ engine.js
@@ -6,9 +6,9 @@ function generateGraph()
   // create an array with nodes
   var nodes = new vis.DataSet([
     { id: 1, label: "A" },
-    { id: 2, label: "Bb" },
-    { id: 3, label: " C" },
-    { id: 4, label: " D " },
+    { id: 2, label: "B" },
+    { id: 3, label: "C" },
+    { id: 4, label: "D" },
     { id: 5, label: "E" },
   ]);
 
