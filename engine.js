37136115b5436911459a9f109f03b206a0d2b19e
Mark Gamble <58432939+Wellingborough@users.noreply.github.com>
2023-07-27 21:42:59 +0100

    Update engine.js

--- engine.js
+++ engine.js
@@ -5,11 +5,11 @@ function generateGraph()
 {
   // create an array with nodes
   var nodes = new vis.DataSet([
-    { id: 1, label: "Node 1" },
-    { id: 2, label: "Node 2" },
-    { id: 3, label: "Node 3" },
-    { id: 4, label: "Node 4" },
-    { id: 5, label: "Node 5" },
+    { id: 1, label: "A" },
+    { id: 2, label: "B" },
+    { id: 3, label: "C" },
+    { id: 4, label: "D" },
+    { id: 5, label: "E" },
   ]);
 
   // create an array with edges
@@ -23,10 +23,27 @@ function generateGraph()
 
   // create a network
   var container = document.getElementById("mynetwork");
+
   var data = {
     nodes: nodes,
     edges: edges,
   };
-  var options = {};
+
+  var options = {
+    nodes: {
+      shape: "dot",
+      size: 30,
+      font: {
+        size: 32,
+      },
+      borderWidth: 2,
+      shadow: true,
+    },
+    edges: {
+      width: 2,
+      shadow: true,
+    },
+  };
+
   var network = new vis.Network(container, data, options);
 }
