9edafa2b2aea19791d91f9a964736ec0ce5df90a
Mark Gamble <58432939+Wellingborough@users.noreply.github.com>
2023-07-28 19:36:26 +0100

    Update engine.js

--- engine.js
+++ engine.js
@@ -5,20 +5,30 @@ function generateGraph()
 {
   // create an array with nodes
   var nodes = new vis.DataSet([
-    { id: 1, label: "AA" },
-    { id: 2, label: "BB" },
-    { id: 3, label: "C" },
-    { id: 4, label: "DD" },
-    { id: 5, label: "E" },
+    { id: 1, label: " A " },
+    { id: 2, label: " B " },
+    { id: 3, label: " C " },
+    { id: 4, label: " D " },
+    { id: 5, label: " E " },
+    { id: 6, label: " F " },
+    { id: 7, label: " G " },
+    { id: 8, label: " H " },
   ]);
 
   // create an array with edges
   var edges = new vis.DataSet([
-    { from: 1, to: 3, label: "12" },
-    { from: 1, to: 2, label: "10" },
-    { from: 2, to: 4, label: "7" },
-    { from: 2, to: 5, label: "14" },
-    { from: 3, to: 3, label: "6" },
+    { from: 1, to: 2, label: "8" },
+    { from: 1, to: 3, label: "5" },
+    { from: 2, to: 4, label: "14" },
+    { from: 2, to: 5, label: "3" },
+    { from: 3, to: 5, label: "10" },
+    { from: 4, to: 5, label: "3" },
+    { from: 4, to: 5, label: "3" },
+    { from: 4, to: 6, label: "7" },
+    { from: 5, to: 6, label: "1" },
+    { from: 5, to: 7, label: "6" },
+    { from: 6, to: 8, label: "4" },
+    { from: 7, to: 8, label: "9" },
   ]);
 
   // create a network
