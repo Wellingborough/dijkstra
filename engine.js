eca3804a89f6493f68153734cd2331f3fcec9f08
Mark Gamble <58432939+Wellingborough@users.noreply.github.com>
2023-07-31 15:56:53 +0100

    Update engine.js

--- engine.js
+++ engine.js
@@ -407,8 +407,8 @@ function stepRoute()
 function copyRow()
 {
   tableRows = document.getElementById("dijkstra-table-body").rows;
-  numRows = rows.length;
-  console.log(JSON.stingify(rows[numRows-1]));
+  numRows = tableRows.length;
+  console.log(JSON.stingify(tableRows[numRows-1]));
 }
 
 function addRow()
